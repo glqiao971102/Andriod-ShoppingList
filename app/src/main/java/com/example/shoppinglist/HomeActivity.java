6489c729c68c38e3befe1e5ce03051b99f07fcf9
glqiao971102 <44012470+glqiao971102@users.noreply.github.com>
2020-06-01 18:27:45 +0800

    Adding the HomePage and the input page
    To create and edit the this 2 page, but the function is not created yet

--- app/src/main/java/com/example/shoppinglist/HomeActivity.java
+++ app/src/main/java/com/example/shoppinglist/HomeActivity.java
@@ -1,14 +1,22 @@
 package com.example.shoppinglist;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+
 public class HomeActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.home_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("MY SUPER SHOPPING LIST");
+
     }
 }
