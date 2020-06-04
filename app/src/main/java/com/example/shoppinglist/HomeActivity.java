b9ec3b6e88035ac4ebcb96fa7284a49365d1b346
glqiao971102 <44012470+glqiao971102@users.noreply.github.com>
2020-06-04 13:36:16 +0800

    revert receive database

--- app/src/main/java/com/example/shoppinglist/HomeActivity.java
+++ app/src/main/java/com/example/shoppinglist/HomeActivity.java
@@ -3,40 +3,29 @@ package com.example.shoppinglist;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+<<<<<<< HEAD
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.shoppinglist.Model.Data;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+=======
 
-import java.text.DateFormat;
-import java.util.Date;
+>>>>>>> parent of 96ac913... Insert data to firebase && design the item page
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class HomeActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private FloatingActionButton fab_btn;
-
-    private DatabaseReference mDatabase;
-    private FirebaseAuth mAuth;
-
-    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,21 +35,6 @@ public class HomeActivity extends AppCompatActivity {
 //        toolbar = findViewById(R.id.home_toolbar);
 //        setSupportActionBar(toolbar);
 //        getSupportActionBar().setTitle("MY SUPER SHOPPING LIST");
-
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser mUser = mAuth.getCurrentUser();
-        String uId = mUser.getUid();
-
-        mDatabase.keepSynced(true);
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Shopping List").child(uId);
-
-        recyclerView = findViewById(R.id.recycler_home);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-
-        layoutManager.setStackFromEnd(true);
-        layoutManager.setReverseLayout(true);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(layoutManager);
 
         fab_btn = findViewById(R.id.fab);
 
@@ -95,8 +69,6 @@ public class HomeActivity extends AppCompatActivity {
                     String myPrice = price.getText().toString().trim();
                     String myNote = note.getText().toString().trim();
 
-                    int intPrice = Integer.parseInt(myPrice);
-
                     if(TextUtils.isEmpty(myType)){
                         type.setError("Hey, cannot leave it blank");
                         return;
@@ -109,15 +81,6 @@ public class HomeActivity extends AppCompatActivity {
                         note.setError("Hey, cannot leave it blank");
                         return;
                     }
-
-                    String id = mDatabase.push().getKey();
-
-                    String date = DateFormat.getDateInstance().format(new Date());
-                    Data data = new Data(myType,intPrice,myNote,date,id);
-
-                    mDatabase.child(id).setValue(data);
-
-                    Toast.makeText(getApplicationContext(),"Item added", Toast.LENGTH_SHORT).show();
 
                     dialog.dismiss();
                 }
